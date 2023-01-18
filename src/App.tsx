754e5af10a4e04cc70d9ba2eeb17e34e60352a53
ars3nka <pznkcommercial@gmail.com>
2023-01-18 14:04:37 +0300

    form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
 import './App.css';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Form } from './components/form';
 
 function App() {
   return (
-    <div className="login-form">
-      <h1>Sign in</h1>
-      <p>Sign in and start managing your candidates!</p>
-      <form action="">
-        <input type="email" id="email" placeholder="Login" />
-        <div className="password">
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            required
-          />
-          <FontAwesomeIcon
-            icon={faEye}
-            size="lg"
-            id="password-control"
-            onClick={() => showHidePassword('password')}
-          />
-        </div>
-        <div className="login-form-rememberme">
-          <div>
-            <input type="checkbox" name="rememberme" id="rememberme" required />
-            <label htmlFor="rememberme">Remember me</label>
-          </div>
-          <a href="">Forgot password?</a>
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div>
+      <Form></Form>
     </div>
   );
-}
-
-function showHidePassword(id: string) {
-  const passInput = document.getElementById(id) as HTMLInputElement;
-  if (passInput.type === 'password') {
-    passInput.type = 'text';
-  } else if (passInput.type === 'text') {
-    passInput.type = 'password';
-  }
 }
 
 export default App;
