de5cf26ec97387f200f564da28c49eeef22e8bde
ars3nka <pznkcommercial@gmail.com>
2023-01-18 12:37:33 +0300

    create login form (lesson3 - task1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-form">
+      <h1>Sign in</h1>
+      <p>Sign in and start managing your candidates!</p>
+      <form action="">
+        <input type="email" id="email" placeholder="Login" />
+        <div className="password">
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+          />
+          <FontAwesomeIcon
+            icon={faEye}
+            size="lg"
+            id="password-control"
+            onClick={() => showHidePassword('password')}
+          />
+        </div>
+        <div className="login-form-rememberme">
+          <div>
+            <input type="checkbox" name="rememberme" id="rememberme" required />
+            <label htmlFor="rememberme">Remember me</label>
+          </div>
+          <a href="">Forgot password?</a>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
+}
+
+function showHidePassword(id: string) {
+  const passInput = document.getElementById(id) as HTMLInputElement;
+  if (passInput.type === 'password') {
+    passInput.type = 'text';
+  } else if (passInput.type === 'text') {
+    passInput.type = 'password';
+  }
 }
 
 export default App;
